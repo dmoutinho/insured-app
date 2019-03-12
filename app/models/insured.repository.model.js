22bdc6d3d23b8aa94592a7d174f938d31a959103
dmoutinho <dmoutinho@gmail.com>
2019-03-12 00:42:15 -0300

    Setting test and init

--- app/models/insured.repository.model.js
+++ app/models/insured.repository.model.js
@@ -1,93 +1,96 @@
 const Insured = require('./insured.model.js');
+
 const insuredList = new Array();
 
+insuredList.push(new Insured({
+    firstName : "João",
+    lastName : "Silva",
+    document : "12345678969",
+    birthday : "1960-01-13",
+    contact : {
+        email : "jsilva@insured.com.br",
+        phone :	"5521999988886"    	    
 
-//module.exports = function(firstName,lastName,birthday,contact,location,payment) {
-
-insuredList.push(new Insured("João",
-    "Silva",
-    "12345678969",
-    "1960-01-13",
-    {
-        email:"jsilva@insured.com.br",
-        phone:"5521999988886"
     },
-    {
+    location : {
         country : "Brasil",
         state : "Rio de Janeiro",
         city : "Rio de Janeiro",
         street : "Rua Cosme Velho",
         number : "513",
         code : "22241125"
-    },
-    {
+    },	    	
+    payment : {
         cardNumber : "9999999999999999", 
         valid : "21-09"
     }
-));
-insuredList.push(new Insured("Maria",
-    "Maria",
-    "12345678977",
-    "1960-06-17",
-    {
+}));
+insuredList.push(new Insured({
+    firstName : "Maria",
+    lastName : "Maria",
+    document : "12345678977",
+    birthday : "1960-06-17",
+    contact : {
         email:"mmaria@insured.com.br",
         phone:"5521999977774"
     },
-    {
+    location : {
         country : "Brasil",
         state : "Rio de Janeiro",
         city : "Niterói",
         street : "Estr. da Viração",
         number : "",
         code : "26177600"
-    },
-    {
+    },	    	
+    payment : {
         cardNumber : "8888888888888888", 
         valid : "20-08"
     }
-));
-insuredList.push(new Insured("José",
-    "Santos",
-    "12345678966",
-    "1950-04-09",
-    {
+}));
+insuredList.push(new Insured({
+    firstName : "José",
+    lastName : "Santos",
+    document : "12345678966",
+    birthday : "1950-04-09",
+    contact : {
         email:"jsantos@insured.com.br",
         phone:"5521999966655"
     },
-    {
+    location : {
         country : "Brasil",
         state : "Rio de Janeiro",
         city : "Rio de Janeiro",
         street : "Av. Pres. Castelo Branco",
         number : "",
         code : "20271130"
-    },
-    {
+    },	    	
+    payment : {
         cardNumber : "7777777777777777", 
         valid : "19-08"
     }
-));
-insuredList.push(new Insured("Francisco",
-    "Sousa",
-    "12345678955",
-    "2005-06-17",
-    {
+}));
+insuredList.push(new Insured({
+    firstName : "Francisco",
+    lastName : "Sousa",
+    document : "12345678955",
+    birthday : "2005-06-17",
+    contact : {
         email:"fsousa@insured.com.br",
         phone:"5521999955555"
     },
-    {
+    location : {
         country : "Brasil",
         state : "Rio de Janeiro",
         city : "Rio de Janeiro",
         street : "R. dos Arcos",
         number : "",
         code : "20230060"
-    },
-    {
+    },	    	
+    payment : {
         cardNumber : "6666666666666666", 
         valid : "19-08"
     }
-));
+}));
 
 exports.create = (insured) => {
     try {
---
+++ test/api/insured.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+(function(){
+    console.log("====================== insured - start ======================");
+    console.log("====================== insured - end ========================")
+})();
+
+process.exit();
