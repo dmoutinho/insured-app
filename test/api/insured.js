a69185040f9e2c73f62cf681c63deaa8f1159e0e
Diogo Moutinho <dmoutinho@gmail.com>
2019-03-14 17:08:16 -0300

    Refactoring

--- test/api/insured.js
+++ test/api/insured.js
@@ -3,119 +3,146 @@ const request = require('request');
 
 (function(){
 
-    describe('Insured', function() {
-    
-        describe('List', function() {
-            it('should return length 4 and http code 200', function() {
-                request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    assert.equal(body.length,4);
-                    assert.equal(res.statusCode,200);
-                });
-            });
-        });
-        
-        describe('Get', function() {
-            it('should return not found message and http code 404', function() {
-                request('http://localhost:3000/insured/123', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    assert.equal(body.message.includes("not found"),true);
-                    assert.equal(res.statusCode,404);
-                });
-            });
-            it('should return specific insured and http code 200', function() {
-                request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    let uuid = body[0].uuid;
-                    request('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
-                        if (err) { return console.log(err); }
-                        assert.equal(body.uuid,uuid);
-                        assert.equal(res.statusCode,200);
-                    });
-                });
-            });    
-        });
-
-        describe('Delete', function() {
-            it('should return not found message and http code 404', function() {
-                request.del('http://localhost:3000/insured/123', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    assert.equal(body.message.includes("not found"),true);
-                    assert.equal(res.statusCode,404);
-                });
-            });
-            it('should return http code 200', function() {
-                request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    let uuid = body[0].uuid;
-                    request.del('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
-                        if (err) { return console.log(err); }
-                        assert.equal(res.statusCode,200);
-                        request('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
-                            if (err) { return console.log(err); }
-                            assert.equal(body.message.includes("not found"),true);
-                            assert.equal(res.statusCode,404);
-                        });
-                    });
-                });
-            });    
-        });
-        
-        describe('Update', function() {
-
-            let data = {
-                firstName : "João",
-                lastName : "Silva",
-                document : "12345678969",
-                birthday : "1960-01-13",
-                contact : {
-                    email : "jsilva@insured.com.br",
-                    phone :	"5521999988886"    	    
-
-                },
-                location : {
-                    country : "Brasil",
-                    state : "Rio de Janeiro",
-                    city : "Rio de Janeiro",
-                    street : "Rua Cosme Velho",
-                    number : "513",
-                    code : "22241125"
-                },	    	
-                payment : {
-                    cardNumber : "9999999999999999", 
-                    valid : "21-09"
-                }
-            };
-
-            it('should return not found message and http code 404', function() {
-                request.put('http://localhost:3000/insured/123', { json: data }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    assert.equal(body.message.includes("not found"),true);
-                    assert.equal(res.statusCode,404);
-                });
-            });
-            it('should return insured and http code 200', function() {
-                request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    let uuid = body[0].uuid;
-                    console.log(uuid)
-                    request.put('http://localhost:3000/insured/'+uuid, { json: data }, (err, res, body) => {
-                        if (err) { return console.log(err); }
-                        console.log(body)
-                        assert.equal(res.statusCode,200);
-
-
-                        // request('http://localhost:3000/insured/'+uuid, { json: data }, (err, res, body) => {
-                        //     if (err) { return console.log(err); }
-                        //     //assert.equal(body.message.includes("not found"),true);
-                        //     assert.equal(res.statusCode,200);
-                        // });
-                    });
-                });
-            });    
-
-        });
-        
+    //List
+    request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        assert.equal(body.length>0);
+        assert.equal(res.statusCode,200);
     });
+
+    //Get not found
+    request('http://localhost:3000/insured/123', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        assert.equal(body.message.includes("not found"),true);
+        assert.equal(res.statusCode,404);
+    });
+
+    //Get specific insured
+    request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        let uuid = body[0].uuid;
+        request('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
+            if (err) assert.fail(err);
+            assert.equal(body.uuid,uuid);
+            assert.equal(res.statusCode,200);
+        });
+    });
+    
+    //Delete not found
+    request.del('http://localhost:3000/insured/123', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        assert.equal(body.message.includes("not found"),true);
+        assert.equal(res.statusCode,404);
+    });
+
+    //Delete specific insured
+    request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        let uuid = body[0].uuid;
+        request.del('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
+            if (err) assert.fail(err);
+            assert.equal(res.statusCode,200);
+            request('http://localhost:3000/insured/'+uuid, { json: true }, (err, res, body) => {
+                if (err) assert.fail(err);
+                assert.equal(body.message.includes("not found"),true);
+                assert.equal(res.statusCode,404);
+            });
+        });
+    });
+
+    let data = {
+        firstName : "João",
+        lastName : "Silva",
+        document : "12345678969",
+        birthday : "1960-01-13",
+        contact : {
+            email : "jsilva@insured.com.br",
+            phone :	"5521999988886"    	    
+
+        },
+        location : {
+            country : "Brasil",
+            state : "Rio de Janeiro",
+            city : "Rio de Janeiro",
+            street : "Rua Cosme Velho",
+            number : "513",
+            code : "22241125"
+        },	    	
+        payment : {
+            cardNumber : "9999999999999999", 
+            valid : "21-09"
+        }
+    };
+
+    //Update not found
+    request.put('http://localhost:3000/insured/123', { json: data }, (err, res, body) => {
+        if (err) assert.fail(err);
+        assert.equal(body.message.includes("not found"),true);
+        assert.equal(res.statusCode,404);
+    });
+
+    //Update specific insured
+    request('http://localhost:3000/insured', { json: true }, (err, res, body) => {
+        if (err) assert.fail(err);
+        let uuid = body[1].uuid;
+        request.put('http://localhost:3000/insured/'+uuid, { json: data }, (err, res, body) => {
+            if (err) assert.fail(err);
+            assert.equal(res.statusCode,200);
+            request('http://localhost:3000/insured/'+uuid, { json: data }, (err, res, body) => {
+                if (err) assert.fail(err);
+
+                assert.equal(body.firstName,"João");
+                assert.equal(body.lastName,"Silva");
+                assert.equal(body.document,"12345678969");
+                assert.equal(body.birthday,"1960-01-13");
+
+                assert.equal(body.contact.email,"jsilva@insured.com.br");
+                assert.equal(body.contact.phone,"5521999988886");
+
+                assert.equal(body.location.country,"Brasil");
+                assert.equal(body.location.state,"Rio de Janeiro");
+                assert.equal(body.location.city,"Rio de Janeiro");
+                assert.equal(body.location.street,"Rua Cosme Velho");
+                assert.equal(body.location.number,"513");
+                assert.equal(body.location.code,"22241125");
+
+                assert.equal(body.payment.cardNumber,"9999999999999999");
+                assert.equal(body.payment.valid,"21-09");
+
+                assert.equal(res.statusCode,200);
+            });
+        });
+    });
+
+    //Create
+    request.post('http://localhost:3000/insured', { json: data }, (err, res, body) => {
+        if (err) assert.fail(err);
+        let uuid = body.uuid;
+        request.get('http://localhost:3000/insured/'+uuid, { json: data }, (err, res, body) => {
+            if (err) assert.fail(err);
+ 
+            assert.equal(body.firstName,"João");
+            assert.equal(body.lastName,"Silva");
+            assert.equal(body.document,"12345678969");
+            assert.equal(body.birthday,"1960-01-13");
+
+            assert.equal(body.contact.email,"jsilva@insured.com.br");
+            assert.equal(body.contact.phone,"5521999988886");
+
+            assert.equal(body.location.country,"Brasil");
+            assert.equal(body.location.state,"Rio de Janeiro");
+            assert.equal(body.location.city,"Rio de Janeiro");
+            assert.equal(body.location.street,"Rua Cosme Velho");
+            assert.equal(body.location.number,"513");
+            assert.equal(body.location.code,"22241125");
+
+            assert.equal(body.payment.cardNumber,"9999999999999999");
+            assert.equal(body.payment.valid,"21-09");
+
+            assert.equal(res.statusCode,200);
+
+        });
+    });
+
 
 })();
--- test/init.js
+++ test/init.js
@@ -1,5 +1,6 @@
+const LogBuilder = require('../app/log/log-builder.js');
+const log = new LogBuilder(__filename);
 const server = require('../server.js');
-
 require('./api/insured.js');
 
 // after(done => {
