4e9552130697b209d0208298b37092a02a7df2e2
Diogo Moutinho <dmoutinho@gmail.com>
2019-03-15 01:07:41 -0300

    Update init.js

--- test/init.js
+++ test/init.js
@@ -1,8 +1,11 @@
 const LogBuilder = require('../app/log/log-builder.js');
 const log = new LogBuilder(__filename);
 const server = require('../server.js');
-require('./api/insured.js');
 
-// after(done => {
+require('./api/insured.test.js');
+require('./api/proposal.test.js');
+
+// after(done proposal {
+
 //     server.close(done);
 // });
