c86d5070bd15d6d19cc56579ffd0d48cf751d20f
Diogo Moutinho <dmoutinho@gmail.com>
2019-03-15 11:12:23 -0300

    Test

--- test/init.test.js
+++ test/init.test.js
@@ -3,9 +3,7 @@ const log = new LogBuilder(__filename);
 const server = require('../server.js');
 
 require('./api/insured.test.js');
-require('./api/proposal.test.js');
 
 // after(done proposal {
-
 //     server.close(done);
 // });
