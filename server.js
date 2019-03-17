d21e74ff33ed2336ecfe5e4cf836781ea6215246
dmoutinho <dmoutinho@gmail.com>
2019-03-17 17:11:41 -0300

    Setting an url base

--- server.js
+++ server.js
@@ -6,19 +6,34 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to insured-app!"});
 });
 
-require('./app/routes/insured.routes.js')(app);
-require('./app/routes/proposal.routes.js')(app);
+// create express insured-app
+const apiInsuredApp = express();
+const apiVersion = "v1"
+const apiContext = "insured-app"
+const apiCotextRoot = apiContext + "/" + apiVersion;
+
+// parse requests of content-type - application/x-www-form-urlencoded
+apiInsuredApp.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+apiInsuredApp.use(bodyParser.json())
+
+require('./app/routes/insured.routes.js')(apiInsuredApp);
+require('./app/routes/proposal.routes.js')(apiInsuredApp);
+
+apiInsuredApp.on('mount', function (parent) {
+    console.log('Admin Mounted');
+    // console.log(parent); // refers to the parent app
+  });
+
+app.use("/"+apiCotextRoot,apiInsuredApp);
+
+console.log(apiInsuredApp);
 
 // listen for requests
 const server = app.listen(3000, () => {
